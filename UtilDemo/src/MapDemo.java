c5baab6d8601622eb5051eae35c6c5dc72c6a7f1
yyc2011 <yyc2015@gmail.com>
2016-04-28 00:14:38 +0800

    测试
    测试

---
+++ UtilDemo/src/MapDemo.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapDemo {
+
+	public static void main(String [] args){
+		Map<String,String> map = new HashMap<String,String>();
+		
+		map.put("key1", "value");
+		map.get("key1");
+		
+	}
+	
+}
