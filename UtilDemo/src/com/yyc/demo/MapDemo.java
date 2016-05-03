9c8c42b18c8aa72806bd74655f699f78eeb66f7d
yyc2011 <yyc2015@gmail.com>
2016-05-04 01:18:58 +0800

    上传HashMap例子类和工作原理说明
    RT

---
+++ UtilDemo/src/com/yyc/demo/MapDemo.java
@@ -0,0 +1,16 @@
+package com.yyc.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapDemo {
+
+	public static void main(String [] args){
+		Map<String,String> map = new HashMap<String,String>();
+		
+		map.put("key1", "value");
+		map.get("key1");
+		
+	}
+	
+}
